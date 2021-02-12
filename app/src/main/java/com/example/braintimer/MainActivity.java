d10e1d93577493f01f3f3a6362205ec3f9f294ba
pauljainta <58131098+pauljainta@users.noreply.github.com>
2021-02-13 02:02:01 +0600

    Timer doesn't show

--- app/src/main/java/com/example/braintimer/MainActivity.java
+++ app/src/main/java/com/example/braintimer/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import androidx.gridlayout.widget.GridLayout;
@@ -18,24 +19,79 @@ public class MainActivity extends AppCompatActivity {
     TextView textView1,textView2,textView3,textView4,timer,question,track,rightwrong;
     GridLayout gridLayout;
     Button button;
-    CountDownTimer countDownTimer;
-    Thread thread;
+    int total_question,total_correct_ans;
+    boolean isCorrect;
 
-//    public void Process(int type)
-//    {
-//
-//        if(type==1)
-//        {
-//
-//
-//        }
-//
-//    }
+
+
+
+    public void boardInit()
+    {
+
+
+           isCorrect=false;
+           track.setText(total_correct_ans+"/"+total_question);
+           Log.i("track","OK"+total_correct_ans+"/"+total_question);
+               Random random=new Random();
+
+
+                   int x = random.nextInt(20);
+                   int y = random.nextInt(10);
+                   int result = x + y;
+                   question.setText(x + "+" + y + "=");
+                   Log.i("ques", "OK" + x + "+" + y + "=");
+                   int res_index = random.nextInt(gridLayout.getChildCount());
+                   TextView res_textView = (TextView) gridLayout.getChildAt(res_index);
+                   res_textView.setText(result + "");
+                   for (int i = 0; i < gridLayout.getChildCount(); i++) {
+                       if (i != res_index) {
+                           TextView textView = (TextView) gridLayout.getChildAt(i);
+                           int fake_res = random.nextInt(30);
+                           while (fake_res == result) {
+                               fake_res = random.nextInt(30);
+                           }
+                           textView.setText(fake_res + "");
+
+
+                       }
+                   }
+
+                   for (int i = 0; i < gridLayout.getChildCount(); i++) {
+                       TextView textView = (TextView) gridLayout.getChildAt(i);
+                       int finalI = i;
+                       textView.setOnClickListener(new View.OnClickListener() {
+                           public void onClick(View view) {
+                               if(!button.getText().toString().equalsIgnoreCase("PLAY AGAIN")) {
+                                   if (finalI == res_index) {
+                                       rightwrong.setText("Right");
+                                       total_correct_ans += 1;
+                                       total_question += 1;
+
+                                   } else {
+                                       rightwrong.setText("Wrong");
+                                       total_question += 1;
+                                   }
+
+                                   track.setText(total_correct_ans + "/" + total_question);
+                                   isCorrect = true;
+                               }
+                           }
+                       });
+                   }
+
+
+
+
+
+
+    }
+
+
 
     public void GO(View view)
     {
           button.setVisibility(View.INVISIBLE);
-//          gridLayout.setVisibility(View.VISIBLE);
+         gridLayout.setVisibility(View.VISIBLE);
           textView1.setVisibility(View.VISIBLE);
           textView2.setVisibility(View.VISIBLE);
           textView3.setVisibility(View.VISIBLE);
@@ -44,32 +100,50 @@ public class MainActivity extends AppCompatActivity {
           question.setVisibility(View.VISIBLE);
           track.setVisibility(View.VISIBLE);
           rightwrong.setVisibility(View.VISIBLE);
-        Random random=new Random();
+
+        total_correct_ans=total_question=0;
+        isCorrect=true;
+
+        rightwrong.setText("");
+        button.setText("GO");
+
+
+
+           Random random=new Random();
 
           int time_limit=10+random.nextInt(30);
 
-          countDownTimer=new CountDownTimer(time_limit*1000,1000) {
+
+
+
+          new CountDownTimer(time_limit*1000,1000) {
 
               @Override
               public void onTick(long millisUntilFinished) {
                   int t= (int) (millisUntilFinished/1000);
                   String time=t+"s";
                   timer.setText(time);
+                 if(isCorrect)
+                  {
+                      boardInit();
+
+                  }
+
+
               }
 
               @Override
               public void onFinish() {
-
                   button.setText("PLAY AGAIN");
                   button.setVisibility(View.VISIBLE);
               }
-          };
+          }.start();
 
-          countDownTimer.start();
 
-//          int x=random.nextInt();
-//          int y=random.nextInt();
-//          int result=x+y;
+
+
+
+
 
      
 
@@ -79,8 +153,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        gridLayout=findViewById(R.id.gridlayout);
         setContentView(R.layout.activity_main);
+        gridLayout=findViewById(R.id.gridlayout);
         textView1=findViewById(R.id.textView1);
         textView2=findViewById(R.id.textView2);
         textView3=findViewById(R.id.textView3);
@@ -90,6 +164,18 @@ public class MainActivity extends AppCompatActivity {
         question=findViewById(R.id.question);
         rightwrong=findViewById(R.id.rightwrong);
         track=findViewById(R.id.track);
+
+        gridLayout.setVisibility(View.INVISIBLE);
+        textView1.setVisibility(View.INVISIBLE);
+        textView2.setVisibility(View.INVISIBLE);
+        textView3.setVisibility(View.INVISIBLE);
+        textView4.setVisibility(View.INVISIBLE);
+        timer.setVisibility(View.INVISIBLE);
+        question.setVisibility(View.INVISIBLE);
+        track.setVisibility(View.INVISIBLE);
+        rightwrong.setVisibility(View.INVISIBLE);
+
+
 
     }
 }
